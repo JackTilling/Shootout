16967a67be30034da514fee533734066060364c9
JackTilling <43538414+JackTilling@users.noreply.github.com>
2018-10-31 19:03:04 +0000

    Main Mechanics Working
    Bottles spawn from left side of the screen, activated using space.

---
+++ Assets/Scripts/BottleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleScript : MonoBehaviour {
+	
+	private bool destroyed;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("LOADED BOTTLE");
+		destroyed = false;
+	}	
+
+	private void OnMouseDown() {
+		if (!destroyed) {
+			destroyed = true;
+			Debug.Log("_Destroyed_");
+
+			GameObject.Find("GameManager").GetComponent<GameManagerScript>().scoreCount++;
+			Debug.Log(GameObject.Find("GameManager").GetComponent<GameManagerScript>().scoreCount);
+		}
+	}
+}
---
+++ Assets/Scripts/BottleSpawnerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleSpawnerScript : MonoBehaviour {
+
+	public GameObject bottleObject;
+
+	private GameObject bottle;
+	private Rigidbody2D rb2D;
+
+	private int xForce;
+	private int yForce;
+
+	// Use this for initialization
+	void Start () {
+		// SpawnBottle();
+	}
+
+	private void Update() {
+		if (Input.GetKeyDown("space"))
+        {
+            SpawnBottle();
+        }
+	}
+
+	void SpawnBottle() {
+		bottle = Instantiate(bottleObject, transform.position, Quaternion.identity);
+		rb2D = bottle.GetComponent<Rigidbody2D>();
+		GetForces();
+		rb2D.isKinematic = false;
+		rb2D.AddForce(new Vector2(xForce,yForce));
+	}
+
+	void GetForces() {
+		xForce = Random.Range(170,230);
+		yForce = Random.Range(90,130);
+	}
+}
